52c3e6eadd1940d44c18dc353b172b931791c52d
Amund Berger Lindstøl <fus007@student.uib.no>
2019-04-09 14:29:40 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+skjule/vise elementer vha klasser
+
+//CSS
+.hidden{
+    display:none;
+}
+
+//JS
+element.classList.add("hidden");
+element.classList.remove("hidden");
+
+//demo
+https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+**/
+
