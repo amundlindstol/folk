8b2a52f8c69dcc3f592d21d22f1b96eb5d3da0bd
Sofia Eika <sei019@student.uib.no>
2019-05-03 20:50:57 +0200

    Lagt til litt på utdanning.js

--- src/js/utdanning.js
+++ src/js/utdanning.js
@@ -3,32 +3,67 @@ class Utdanning {
     constructor(url){
         this.onload = null; //skal potensielt tilordnes en funksjon
         this.url = url;
-        this.dataAccessor = new Data(url); 
+        this.dataAccessor = new Data(url);
+        this.load();
+        this.getHigherEducation();
     }
 
     //Fra oppgbeskrivelse
     /**
      * Returnerer listen av alle kommunenavn som fremtrer i datasettene.
-     * 
+     *
      * @return names
      */
+    getNames(){
+      var elementer = this.data;
+      var ret = [];
+      var count = 0;
+      for (var kommune in elementer) {
+          ret[count++] = kommune;
+      }
+      return ret;
+    }
 
-    getNames(){}
     /**
      * Returnerer listen av kommunenummer som fremtrer i datasettene.
-     * 
+     *
      * @return numbers
      */
-    getIDs(){}
-    /**
-     * Returnerer informasjon om en kommune basert på aktuelt datasett og gitt kommuneNr
-     * 
-     * @return numbers
-     */
-    getInfo(number){}
+    getIDs(){
+      var elementer = this.data;
+      var ret = [];
+      var count = 0;
+      for (var kommune in elementer) {
+          ret[count++] = String(elementer[kommune].kommunenummer);
+      }
+      return ret;
+    }
+
+
+    getKommuneByID(nummer) {
+      for (var kommune in this.data) {
+        if (nummer == this.data[kommune].kommunenummer) {
+          var obj = this.data[kommune]
+          obj["name"] = kommune;
+          return obj;
+        }
+      }
+    }
+
+    //03a + 04a (høyere utdanning)
+    //uferdig metode
+    getHigherEducation(){
+        var ret = {};
+        for(var kommune in this.data){
+            var tempKommune = this.getKommuneByID(this.data[kommune].kommunenummer);
+            ret[kommune] = tempKommune;
+        }
+        return ret;
+    }
 
 
     load(){
-        this.data = this.dataAccessor.accessData();
+        this.dataAccessor.accessData();
+        this.data = this.dataAccessor.data;
     }
 }
