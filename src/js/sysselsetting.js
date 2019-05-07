dc98cf12dfbe70d4018a4200495af0064588372e
eivinddh <eivind.d.halderaker@hotmail.com>
2019-05-07 14:17:41 +0200

    Removed TODO, because it's done

--- src/js/sysselsetting.js
+++ src/js/sysselsetting.js
@@ -29,7 +29,6 @@ class Sysselsetting {
 
     /**
      * Klargjør og sender en forespørsel om å laste ned datasettet. 
-     * To do: "Dersom objektet har egenskapet onload.."
      */
     load() {
         this.dataAccessor.accessData(this.onload);
