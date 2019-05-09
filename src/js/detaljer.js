eb847b00ee825f871221bfddea1f8d5ad63c2743
Hjalmar S. Andersen <31273368+han036@users.noreply.github.com>
2019-05-09 14:26:19 +0200

    idk hva jeg gjorde

--- src/js/detaljer.js
+++ src/js/detaljer.js
@@ -6,8 +6,8 @@ const hentDetaljer = (befolkning, utdanning, sysselsetting) => {
     var dom = document.getElementById('detaljObjekt');
     dom.innerHTML = "<b>" + kommune.name + "</b><br>" +
         "Kommunenummer: " + kommune.kommunenummer + "<br>" +
-        "Befolkning: " + befolkning.getLastPopulationString(kommune.name);
-
+        "Befolkning: " + befolkning.getLastPopulationString(kommune.name) + "<br>" +
+        "Sysselsetting"+ sysselsetting.getLastEmployment(kommune);
     /**
      * Kommunens navn
      * Kommunenr
